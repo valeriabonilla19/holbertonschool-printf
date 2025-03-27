d911aafd426eefd56e09d88bd90f762956645d1a
Jayvan <jayvanramos23@gmail.com>
2025-03-27 10:37:15 -0400

    editing printf

--- _printf.c
+++ _printf.c
@@ -1,45 +1,50 @@
 #include "main.h"
 
 /**
-* _printf - Function that mimics the printf behavior.
-* @format: The format string.
-* Return: The number of characters printed (excluding the null byte).
-*/
+ * _printf - Function that mimics printf behavior.
+ * @format: The format string.
+ * Return: The number of characters printed (excluding null byte).
+ */
 int _printf(const char *format, ...)
 {
-va_list args;
-int i = 0, count = 0;
-int (*func)(va_list);
+    va_list args;
+    int i = 0, count = 0;
+    int (*func)(va_list);
 
-va_start(args, format);
+    if (!format)
+        return (-1);
 
-while (format && format[i])
-{
-if (format[i] == '%' && format[i + 1] != '\0')
-{
-i++;
-if (format[i] == 'c')
-func = print_char;
-else if (format[i] == 's')
-func = print_string;
-else if (format[i] == '%')
-func = print_percent;
-else
-func = NULL;
+    va_start(args, format);
 
-if (func)
-count += func(args);
-else
-count += write(1, &format[i - 1], 1);
-}
-else
-{
-count += write(1, &format[i], 1);
-}
-i++;
-}
+    while (format[i])
+    {
+        if (format[i] == '%' && format[i + 1] != '\0')
+        {
+            i++;
+            if (format[i] == 'c')
+                func = print_char;
+            else if (format[i] == 's')
+                func = print_string;
+            else if (format[i] == '%')
+                func = print_percent;
+            else
+                func = NULL;
 
-va_end(args);
-return (count);
+            if (func)
+                count += func(args);
+            else
+            {
+                count += write(1, "%", 1);
+                count += write(1, &format[i], 1);
+            }
+        }
+        else
+            count += write(1, &format[i], 1);
+
+        i++;
+    }
+
+    va_end(args);
+    return (count);
 }
 
