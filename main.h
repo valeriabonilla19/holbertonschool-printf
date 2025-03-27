a2ebfbff838f4954af539e8c2ab8f66478e0009a
Jayvan <jayvanramos23@gmail.com>
2025-03-27 11:01:57 -0400

    edited main.h

--- main.h
+++ main.h
@@ -1,13 +1,26 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef Main_H
+#define Main_H
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
-#include <unistd.h>
-
+/**
+ * struct print - prints anything
+ * @type: type specifier
+ * @f: function to execute
+ */
+struct print
+{
+	char *valid;
+	int (*f)(va_list args);
+};
 int _printf(const char *format, ...);
-int print_char(va_list args);
-int print_string(va_list args);
-int print_percent(va_list args);
-
-#endif /* MAIN_H */
-
+int _print_chr(va_list args);
+int _print_num(va_list args);
+int _print_str(va_list args);
+int _putchar(char c);
+int _print_hxs(va_list args);
+int _print_hxl(va_list args);
+int _print_oct(va_list args);
+int (*get_func(char x))(va_list);
+typedef struct print spec;
+#endif
