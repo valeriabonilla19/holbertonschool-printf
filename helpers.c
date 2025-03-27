0e88affb963fe1482669f40c70b659087ca6b2b3
aureliopagan <aurepgst@gmail.com>
2025-03-27 14:24:56 -0400

    Updating

--- helpers.c
+++ helpers.c
@@ -23,17 +23,12 @@ int print_string(va_list args)
 
     /* If the string is NULL, print "(null)" instead */
     if (str == NULL)
-    {
         str = "(null)";
-    }
 
     /* Print the string */
-    while (str[count] != '\0')
+    while (str[count])
     {
-        if (write(1, &str[count], 1) == -1)
-        {
-            return (-1); /* Return -1 on write error */
-        }
+	write(1, &str[count], 1);
         count++;
     }
 
@@ -49,5 +44,53 @@ int print_percent(va_list args)
 {
     (void)args;  /* Unused argument */
     return (write(1, "%", 1));
+}
+
+
+/**
+ * print_integer - Prints an integer.
+ * @args: The argument list.
+ * Return: The number of characters printed.
+ */
+int print_integer(va_list args)
+{
+    int num;  /* Variable to store the integer */
+    int count = 0;  /* To count the number of characters printed */
+    char buffer[10];  /* Buffer to store digits of the number */
+    int i = 0;  /* Index for the buffer */
+    int j;  /* Loop variable for reversing the digits */
+
+    num = va_arg(args, int);  /* Get the integer argument */
+
+    /* Handle the case when the number is zero */
+    if (num == 0)
+    {
+        return write(1, "0", 1);
+    }
+
+    /* Handle negative numbers */
+    if (num < 0)
+    {
+        write(1, "-", 1);
+        num = -num;
+        count++;
+    }
+
+    /* Convert the integer to a string (store in reverse order) */
+    while (num > 0)
+    {
+        buffer[i] = (num % 10) + '0';
+        num = num / 10;
+        i++;
+    }
+
+    /* Print the digits in correct order (reverse the string) */
+    for (j = i - 1; j >= 0; j--)
+    {
+        write(1, &buffer[j], 1);
+        count++;
+    }
+
+    return count;
 }
 
